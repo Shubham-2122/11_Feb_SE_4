5dcfda8b0040bc91fcc0e119c023f0f35fde8a7a
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-02-24 17:06:12 +0530

    c

---
+++ C/08_string/01_string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+	char name1 = 'h';
+	char name2[20] = "Harshil patel";
+	
+	printf("Name : %c",name1);
+	printf("\nName : %s",name2);
+}
---
+++ C/08_string/intro.c
@@ -0,0 +1,13 @@
+ /*
+ 
+ string : collection of  muliple char store . memeroy store 
+ 
+ 	char 1 bits
+	char name = 'Malav' ;
+ 
+ 	char str[10] = 'Malav';
+ 	
+ 	%s
+ 	%c
+ 	
+ */
